231e547eeafa7c7736a8ad909858be018a5d61d1
hastenboom <42961312+hastenboom@users.noreply.github.com>
2019-05-20 19:08:46 +0800

    Update creadit.R

--- creadit.R
+++ creadit.R
@@ -1,1 +1,31 @@
+setwd("C:/Users/Administrator/Desktop/credit")
+
+xdata=read.csv('creditdata.csv',head=T,stringsAsFactors=F)
+
+n=nrow(xdata)
+
+trdata=xdata[1:(n/2),]
+tsdata=xdata[(n/2+1):n,]
+
+reg1=glm(default~.,data=trdata,family=binomial(link = "logit"))
+
+reg2=glm(default~age+gender+marriage1+marriage2+counttime+consume12+consume1,
+         data=trdata,family=
+           binomial(link = "logit"))
+
+pred1=predict(reg1,tsdata,type='response')
+
+pred2=predict(reg2,tsdata,type='response')
+
+theta=sum(trdata$default)/nrow(trdata)
+
+y=tsdata$default
+
+y_hat1=(pred1>theta)
+  y_hat2=(pred2>theta)
+
+cpNP=function(y,y_hat){
+  
+  return(NP)
+}
 
